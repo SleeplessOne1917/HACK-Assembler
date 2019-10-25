12f63a77c9b6c3d7153a7f2ae70fde2c75f8ae69
abias <abias@emoneyadvisor.com>
2019-10-25 11:45:30 -0400

    Delete code to fix file tracking in Git

--- parts/code.py
+++
@@ -1,7 +0,0 @@
-from bitstring import BitArray
-
-
-def dest(mnemonic):
-    if not mnemonic:
-        return BitArray(bin='000')
-    return BitArray(b'').join([bin(reg in mnemonic) for reg in 'ADM'])

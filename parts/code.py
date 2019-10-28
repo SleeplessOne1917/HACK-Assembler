6b6b440052dbd5a9da9d91d268c53d8424eb93c0
abias <kekkekK3KK3K>
2019-10-28 11:19:31 -0400

    Decode jump mnemonics into bits

--- parts/code.py
+++ parts/code.py
@@ -3,3 +3,9 @@ from bitstring import BitArray
 
 def dest(mnemonic):
     return BitArray(b'').join([bin(reg in mnemonic) for reg in 'ADM']) if mnemonic else BitArray(bin='000')
+
+
+def jump(mnemoic):
+    jump_bits = {None: '000', 'JGT': '001', 'JEQ': '010', 'JGE': '011', 'JLT': '100', 'JNE': '101', 'JLE': '110',
+     'JMP': '111'}
+    return BitArray(bin=jump_bits[mnemoic])
--- parts/test_code.py
+++ parts/test_code.py
@@ -1,4 +1,4 @@
-from parts.code import dest
+from parts.code import dest, jump
 from bitstring import BitArray
 from pytest import mark
 
@@ -15,3 +15,18 @@ dest_test_data = [(None, '000'),
 @mark.parametrize('mnemonic,bits', dest_test_data)
 def test_dest_when_mnemonic_has_registers_returns_bits(mnemonic, bits):
     assert dest(mnemonic) == BitArray(bin=bits)
+
+
+jump_test_data = [(None, '000'),
+                  ('JGT', '001'),
+                  ('JEQ', '010'),
+                  ('JGE', '011'),
+                  ('JLT', '100'),
+                  ('JNE', '101'),
+                  ('JLE', '110'),
+                  ('JMP', '111')]
+
+
+@mark.parametrize('mnemonic,bits', jump_test_data)
+def test_jump_when_mnemonic_has_jumps_returns_bits(mnemonic, bits):
+    assert jump(mnemonic) == BitArray(bin=bits)
